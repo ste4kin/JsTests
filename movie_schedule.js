071ef6b65b2c45e9c6718cd4b596133b0e92efb5
ste4kin <vasily_ab@mail.ru>
2014-07-02 10:37:25 +0400

    Create movie_schedule.js

---
+++ movie_schedule.js
@@ -0,0 +1,17 @@
+var movie1 = {
+	title: "plan 9",
+	genre: "cult classic",
+	rating: 5,
+	showtime: ["3:00pm", "7:00pm", "11:00pm"]
+};
+
+var movie2 = {
+	title: "forbidden planet",
+	genre: "classic sci-fi",
+	rating: 3,
+	showtime: ["5:00pm", "9:00pm"]
+};
+
+function getNextShowing(movie) {
+	function getNextShowing
+}
