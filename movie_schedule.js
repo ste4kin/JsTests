b9653989e4f6b850d4d775161b379888f32a6fe5
ste4kin <vasily_ab@mail.ru>
2014-07-03 10:22:22 +0400

    Update movie_schedule.js

--- movie_schedule.js
+++ movie_schedule.js
@@ -1,38 +1,42 @@
-var movie1 = {
-	title: "plan 9",
-	genre: "cult classic",
-	rating: 5,
-	showtimes: ["3pm", "7pm", "11pm"]
-};
+<!DOCTYPE HTML>
+<html>
+  <head> </head>
+  <body>
 
-var movie2 = {
-	title: "forbidden planet",
-	genre: "classic sci-fi",
-	rating: 3,
-	showtimes: ["5:00pm", "9:00pm"]
-};
+    <script>
 
-function getNextShowing(movie) {
-	var now = new Date().getTime();
-	
-	for (var i = 0; i<movie.showtimes.length; i++) {
-		var showtime = getTimeFromString(movie.showtimes[i]);
-		if ((showtime - now) > 0) {
-			return "Next showing of " + movie.title + " is " + movie.showtimes[i]);
-		}
-	}
-	return null;
-}
+      var movie1 = {
+        title: "plan 9",
+        genre: "cult classic",
+        rating: 5,
+        showtimes: ["3:00 pm", "7:00 pm", "11:00 pm"],
+      
+        getNextShowing: function() {
+            var now = new Date().getTime();
+            
+            for (var i = 0; i<this.showtimes.length; i++) {
+                var showtime = getTimeFromString(this.showtimes[i]);
+                if ((showtime - now) > 0) {
+                    return "Next showing of " + this.title + " is " + this.showtimes[i];
+                }
+              
+            }
+            return null;
+        }
+      };
+      
+      function getTimeFromString(timeString) {
+        var theTime = new Date();
+        var time = timeString.match(/(\d+) (?::(\d\d))?\s*(p?)/);
+        theTime.setHours( parseInt(time[1]) + (time[3] ? 12 : 0) );
+        theTime.setMinutes( parseInt(time[2]) || 0 );
+        return theTime.getTime();
+      }
+      
+      var nextShowing = movie1.getNextShowing();
+      alert(nextShowing);
+      	
+    </script>
 
-function getTimeFromString(timeString) {
-  var theTime = new Date();
-  var time = timeString.match(/(\d+) (?::(\d\d))?\s*(p?)/);
-  theTime.setHours( parseInt(time[1]) + (time[3] ? 12 : 0) );
-  theTime.setMinutes( parseInt(time[2]) || 0 );
-  return theTime.getTime();
-}
-
-var nextShowing = getNextShowing(movie1);
-alert(nextShowing);
-nextShowing = getNextShowing(movie2);
-alert(nextShowing);
+  </body>
+</html>
